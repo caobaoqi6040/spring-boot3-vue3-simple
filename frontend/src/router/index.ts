62ee2c3619b9836897b1cffe157950d73a86f245
caobaoqi6040 <caobaoqi6040@gmail.com>
2025-10-28 15:28:57 +0800

    :sparkles: feat(dev-ops): 初始化 flex and grid

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,28 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import GridView from "@/views/GridView.vue";
+import FlexView from "@/views/FlexView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
-      path: '/users',
-      name: 'users',
-      component: () => import('../views/UserListView.vue'),
-    },
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView,
+        },
+        {
+            path: '/flex',
+            name: 'flex',
+            component: FlexView,
+        },
+        {
+            path: '/grid',
+            name: 'grid',
+            component: GridView,
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import('../views/AboutView.vue'),
+        },
+        {
+            path: '/users',
+            name: 'users',
+            component: () => import('../views/UserListView.vue'),
+        },
+    ],
 })
 
 export default router
---
+++ frontend/src/views/FlexView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>flex view</h1>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
